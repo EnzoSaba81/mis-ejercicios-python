31138750fe362c7db41c06267fe8eb243f04a21b
Enzo Saba <enzosaba81@gmail.com>
2026-02-24 20:53:25 -0300

    Final: urrency converter fully functional with error handling and loop

--- API/euro-converter-api.py
+++ API/euro-converter-api.py
@@ -11,25 +11,32 @@ datos = respuesta.json()["rates"]
 # Menu para poder elegir 3 opciones de conversion de euro a otras monedas. 
 # El usuario ingresa la cantidad de euros que desea convertir y el programa muestra el resultado de la conversión.
 
-print("Bienvenido ingresa una de las siguientes opciones para convertir de Euro a las siguentes monedas : \n 1. Dólares (USD) \n 2. Libras Esterlinas (GBP) \n 3. Yen Japonés (JPY)  ")
-opcion= input("Ingrese la opción deseada: ").strip()
+while True:
+    print("Bienvenido ingresa una de las siguientes opciones para convertir de Euro a las siguentes monedas : \n 1. Dólares (USD) \n 2. Libras Esterlinas (GBP) \n 3. Yen Japonés (JPY)  ")
+    opcion= input("Ingrese la opción deseada: ").strip()
 
-match opcion:
-    case "1":
-        moneda = "USD"
-    case "2":
-        moneda = "GBP"
-    case "3":
-        moneda = "JPY"
-    case _:
-        moneda = None 
+    if opcion == "4": 
+        print("Saliendo del programa.")
+        break
+    
+    match opcion:
+        case "1":
+            moneda = "USD"
+        case "2":
+            moneda = "GBP"
+        case "3":
+            moneda = "JPY"
+        case _:
+            moneda = None 
         
-if moneda != None:
-    cantidad = float(input(f"Ingresa la cantidad de Euros para convertir a {moneda}: ")) #Segun la opcion que elija el usuario, se le pedira ingresar la cantidad de euros a convertir a la moneda seleccionada.
-    
-    resultado = cantidad * datos[moneda]
-    
-    print(f"Resultado: {cantidad} EUR = {resultado:.2f} {moneda}")# Finalmente, se muestra el resultado de la conversión al usuario.
-else:
-    print("Opción no válida. Por favor, elige una opción del 1 al 3.")
+    if moneda != None:
+        try:
+            cantidad = float(input(f"Ingresa la cantidad de Euros para convertir a {moneda}: "))  #Segun la opcion que elija el usuario, se le pedira ingresar la cantidad de euros a convertir a la moneda seleccionada.
+            resultado = cantidad * datos[moneda]
+            print(f"Resultado: {cantidad} EUR = {resultado:.2f} {moneda}")  # Finalmente, se muestra el resultado de la conversión al usuario.
+        except ValueError:
+            # Si el usuario ingresa texto, se ejecuta esto:
+            print(" Error: Por favor, ingresa solo números.")
+    else:
+        print("Opción no válida. Por favor, elige una opción del 1 al 3.")
 
